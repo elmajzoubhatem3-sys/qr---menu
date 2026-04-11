794bf42b8104041b8198df544acbb1c65f461990
Talal <elmajzoubhatem3@gmail.com>
2026-04-11 03:08:21 +0300

    fix private blob upload

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,23 +1,22 @@
 import { put } from "@vercel/blob";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function POST(req: Request) {
-  const formData = await req.formData();
-  const file = formData.get("file") as File | null;
-
-  if (!file) {
-    return NextResponse.json({ error: "No file" }, { status: 400 });
-  }
-
   try {
-    const blob = await put(file.name, file, {
-      access: "public",
-    });
+    const formData = await req.formData();
+    const file = formData.get("file") as File | null;
+
+    if (!file) {
+      return NextResponse.json({ error: "No file" }, { status: 400 });
+    }
+
+    const blob = await put(file.name, file);
 
     return NextResponse.json({
       url: blob.url,
     });
-
   } catch (err) {
     console.error(err);
     return NextResponse.json(
